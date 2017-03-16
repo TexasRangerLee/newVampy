c7ce6feb8c8006dd3057ecd3353aee3474330a94
calebgeorge3 <calebgeorge3@gmail.com>
2017-03-16 15:54:07 -0500

    Put in wires/powered-unpowered script; put together basics of testing level for demo
    hey clark, we need to talk about your character controller; jumping
    against things is screwy as fuck, if you jump while moving forward
    against a wall, nothing will happen.  if you just jump and stay still,
    you'll climb it and can hit forward at the top to get over the edge.
    also, jumping at a wall and holding forward can get you to stick to it.
    you can't sprint-jump, but if you time it perfectly, you can use it to
    lock sprint on without holding sprint.  there's also the left/right
    oddity we discussed, may be related.
    
    if you wanna test, i have the absolute beginning of the test area put
    together; a speed tunnel and some stairs you can climb
    
    also, if you can think of a structure to put in there, go ahead and do
    it, the more we show off the better

---
+++ Assets/Caleb/Caleb's Stuff All/Scripts/PowerToggleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerToggleScript : MonoBehaviour {
+    public Material powered;
+    public Material unpowered;
+    public Renderer rend;
+    public bool isPowered;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    //if cable is unpowered, change to powered;
+    //if cable is powered, change to unpowered
+    //call this whenever a power/cable system toggles
+    public void changePowerState()
+    {
+        if (isPowered)
+        {
+            isPowered = false;
+            rend.material = unpowered;
+        }
+        else
+        {
+            isPowered = true;
+            rend.material = powered;
+        }
+    }
+}
